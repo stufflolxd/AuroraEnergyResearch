cca25f208bb4004ca8bcb4bd33c72a7a8d6fab86
stufflolxd <99693244+stufflolxd@users.noreply.github.com>
2023-01-16 15:55:13 -0600

    added more credits

--- shared/BridgeNet.lua
+++ shared/BridgeNet.lua
@@ -1,2 +1,7 @@
--- https://github.com/ffrostflame/BridgeNet
--- Not going to bother putting the code here. Just check it out.
+--[[
+    
+https://github.com/ffrostflame/BridgeNet
+https://ffrostflame.github.io/BridgeNet/
+Not going to bother putting the code here. Just check it out.
+
+]]
